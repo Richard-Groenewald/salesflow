21f069a116ffd185774e668774bdc0bb891e9b9e
Richard Groenewald <richard.groenewald@xone.co.za>
2026-05-15 15:34:49 +0200

    Delete StartSalesFlow.bat

--- StartSalesFlow.bat
+++
@@ -1,7 +0,0 @@
-echo.
-echo  SalesFlow server starting on port 8080...
-echo  Keep this window open while using SalesFlow.
-echo.
-timeout /t 1 >nul
-start "" "http://localhost:8080"
-node server.js
